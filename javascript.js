b9d4abd9d6a0a2f5b75e765b39d9e27a2bafefc7
E R M <87425235+iAmCriptic@users.noreply.github.com>
2023-12-13 18:30:36 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,69 @@
+function openAnleitung() {
+    window.location.href = 'Anleitung.html';
+    }
+function openGame(){
+    window.location.href = 'game.html'
+}
+function openHauptseite(){
+    window.location.href = 'Hauptseite.html'
+}
+
+var clicks = 0;
+
+
+function qAlter(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicks > 5){
+      document.getElementById("deinemom").innerHTML= "jetzt";
+  }
+}
+function qHabseligkeiten(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicks > 5){
+      document.getElementById("deinemom").innerHTML= "jetzt";
+  }
+}
+function qWissen(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicks > 5){
+      document.getElementById("deinemom").innerHTML= "jetzt";
+  }
+}
+function qBesuchsgrund(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicks > 5){
+      document.getElementById("deinemom").innerHTML= "jetzt";
+  }
+}
+function qGesundheitsstatus(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicks > 5){
+      document.getElementById("deinemom").innerHTML= "jetzt";
+  }
+}
+function qWiederholterBesuch(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicks > 5){
+      document.getElementById("deinemom").innerHTML= "jetzt";
+  }
+}
+function qBeruf(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicks > 5){
+      document.getElementById("deinemom").innerHTML= "jetzt";
+  }
+}
+function qTest(){
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+  if (clicks > 5){
+    document.getElementById("deinemom").innerHTML= "jetzt";
+}
+}
