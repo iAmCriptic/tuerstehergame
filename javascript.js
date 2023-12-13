ef09c73c64c379451df0579641226822d21ca0e7
iAmCriptic <e.r.matzke@outlook.de>
2023-12-13 19:30:44 +0100

    Funktionalitäten und Antworten Ergänzt

--- javascript.js
+++ javascript.js
@@ -9,61 +9,70 @@ function openHauptseite(){
 }
 
 var clicks = 0;
+var life = 0;
 
 
 function qAlter(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-    if (clicks > 5){
-      document.getElementById("deinemom").innerHTML= "jetzt";
+    document.getElementById("g-person-aussage").innerHTML= "Ich bin 24";
+    if (clicks >= 5){
+      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
   }
 }
 function qHabseligkeiten(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-    if (clicks > 5){
-      document.getElementById("deinemom").innerHTML= "jetzt";
+    document.getElementById("g-person-aussage").innerHTML= "Ich habe mein Portmorne, meine Schlüssel, mein Handy und einen Glücksbringer bei.";
+    if (clicks >= 5){
+      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
   }
 }
 function qWissen(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-    if (clicks > 5){
-      document.getElementById("deinemom").innerHTML= "jetzt";
+    document.getElementById("g-person-aussage").innerHTML= "Nicht viel, er soll gut sein und ich wollte mich davon überzeugen.";
+    if (clicks >= 5){
+      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
   }
 }
 function qBesuchsgrund(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-    if (clicks > 5){
-      document.getElementById("deinemom").innerHTML= "jetzt";
+    document.getElementById("g-person-aussage").innerHTML= "Ich wollte meinen Abend nicht alleine Zuhause Verbringen.";
+    if (clicks >= 5){
+      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
   }
 }
 function qGesundheitsstatus(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-    if (clicks > 5){
-      document.getElementById("deinemom").innerHTML= "jetzt";
+    document.getElementById("g-person-aussage").innerHTML= "Ich bin Gesund.";
+    if (clicks >= 5){
+      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
   }
 }
 function qWiederholterBesuch(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-    if (clicks > 5){
-      document.getElementById("deinemom").innerHTML= "jetzt";
+    document.getElementById("g-person-aussage").innerHTML= "Nein wie Gesagt, das ist Mein erster Besuch";
+    if (clicks >= 5){
+      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
   }
 }
 function qBeruf(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-    if (clicks > 5){
-      document.getElementById("deinemom").innerHTML= "jetzt";
+    document.getElementById("g-person-aussage").innerHTML= "Ich arbeite als Drogendealer";
+    if (clicks >= 5){
+      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
   }
 }
 function qTest(){
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
-  if (clicks > 5){
-    document.getElementById("deinemom").innerHTML= "jetzt";
-}
+  document.getElementById("g-person-aussage").innerHTML= "Entwickleroption, zum Testen von Funktionalitäten.";
+  if (clicks >= 5){
+    document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
+  }
 }
