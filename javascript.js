d674ae642574962ba28879d9e11a5f5a885bb4a2
iAmCriptic <e.r.matzke@outlook.de>
2023-12-14 15:44:54 +0100

    Lebensanzeige, Loose Weiterleitung

--- javascript.js
+++ javascript.js
@@ -9,70 +9,81 @@ function openHauptseite(){
 }
 
 var clicks = 0;
-var life = 0;
+let lives = 3;
 
 
 function qAlter(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("g-person-aussage").innerHTML= "Ich bin 24";
-    if (clicks >= 5){
-      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
-  }
+    ZuVieleFragen();
 }
+
 function qHabseligkeiten(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("g-person-aussage").innerHTML= "Ich habe mein Portmorne, meine Schlüssel, mein Handy und einen Glücksbringer bei.";
-    if (clicks >= 5){
-      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
-  }
+    ZuVieleFragen();
 }
+
 function qWissen(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("g-person-aussage").innerHTML= "Nicht viel, er soll gut sein und ich wollte mich davon überzeugen.";
-    if (clicks >= 5){
-      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
-  }
+    ZuVieleFragen();
 }
+
 function qBesuchsgrund(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("g-person-aussage").innerHTML= "Ich wollte meinen Abend nicht alleine Zuhause Verbringen.";
-    if (clicks >= 5){
-      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
-  }
+    ZuVieleFragen();
 }
+
 function qGesundheitsstatus(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("g-person-aussage").innerHTML= "Ich bin Gesund.";
-    if (clicks >= 5){
-      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
-  }
+    ZuVieleFragen();
 }
+
 function qWiederholterBesuch(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("g-person-aussage").innerHTML= "Nein wie Gesagt, das ist Mein erster Besuch";
-    if (clicks >= 5){
-      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
-  }
+    ZuVieleFragen();
 }
+
 function qBeruf(){
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("g-person-aussage").innerHTML= "Ich arbeite als Drogendealer";
-    if (clicks >= 5){
-      document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
-  }
+    ZuVieleFragen();
 }
+
 function qTest(){
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
   document.getElementById("g-person-aussage").innerHTML= "Entwickleroption, zum Testen von Funktionalitäten.";
+  ZuVieleFragen();
+}
+
+function ZuVieleFragen(){ //Funktion zum Abziehen der Leben bei zu Vielen Fragen
   if (clicks >= 5){
     document.getElementById("g-person-aussage").innerHTML= "Das wird mir zu Blöd, Schönen Tag noch!";
+     // Falsch beantwortet
+    lives--;
+    updateLives();
+
   }
 }
+
+function updateLives() { //Funktion zum Aktualisieren der Leben
+  const livesElement = document.getElementById('lives');
+  livesElement.textContent = lives;
+  if (lives === 0) {
+    // Keine Leben mehr übrig, Spiel beenden oder weitere Aktionen durchführen
+    location.href = 'Loose-Screen.html' //weiterleitung zum loose Screen
+}
+}
+
